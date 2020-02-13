45c16b8da4eb6a23654e855a722e64048cbd5ef5
Adrian <adrian.michalke@yahoo.de>
2020-02-13 18:09:40 +0000

    fix error in power measurements with restarts

--- impl/benchmark/zmq_measure.py
+++ impl/benchmark/zmq_measure.py
@@ -1,9 +1,9 @@
 import zmq
 import subprocess, signal
 import sys
-import os
+import os, time
 
-prog = "~/efficient-gpu-joins/power_measurement/tools/arm-probe/arm-probe/arm-probe"
+prog = "/home/pi/efficient-gpu-joins/power_measurement/tools/arm-probe/arm-probe/arm-probe -d 5"
 
 if len(sys.argv) != 1:
     print("usage: python ./zmq_measure")
@@ -16,22 +16,42 @@ print("Connecting..")
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
 
+
 while(True):
-     #  Get the reply.
+    #  Get the reply.
     print("Waiting for start signal")
-    message = socket.recv()
+    try:
+        message = socket.recv()
+    except: # possible measure problem
+        socket.send(b"Restart")
+        print("Restart")
+        continue
 
     if (message == "Start"):
         p = subprocess.Popen([prog + " > /tmp/measure.txt"], stdout=subprocess.PIPE,shell=True, preexec_fn=os.setsid)
 
         socket.send(b"Started")
+        time.sleep(3)
 
         #  Get the reply.
         print("Waiting for stop signal")
-        message = socket.recv()
+        try:
+            message = socket.recv()
+        except: # possible measure problem
+            socket.send(b"Restart")
+            print("Restart")
+            continue
+
 
         print("Kill measure")
-        os.killpg(p.pid, signal.SIGTERM)
+        #os.killpg(p.pid, signal.SIGTERM)
+        os.kill(p.pid, signal.SIGTERM)
+        for i in range(0,40):
+            try:
+                os.kill(p.pid+i, signal.SIGTERM)
+            except:
+                pass
+        print(p.pid)
 
         f = open("/tmp/measure.txt",'rb')
         fl = f.read()
